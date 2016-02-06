d1e9fc2ed35fbf743fc6d2b896c32b9a307dd312
Sean McNally <seanmcnally88@gmail.com>
2016-02-06 15:06:57 -0600

    Formatted patient step models and controllers, made first step view

--- app/controllers/patient/steps_controller.rb
+++ app/controllers/patient/steps_controller.rb
@@ -1,7 +1,32 @@
 class Patient::StepsController < ApplicationController
+  include Wicked::Wizard
+  steps *Patient.form_steps
+
   def show
+    @patient = Patient.find(params[:patient_id])
+    render_wizard
   end
 
   def update
+    @patient = Patient.find(params[:patient_id])
+    @patient.update(patient_params(step))
+    render_wizard @patient
   end
+
+  private
+
+  def patient_params(step)
+    permitted_attributes = 
+      case step
+      when "basic"
+        [:first_name, :last_name, :address_1, :address_2, :city, :state, :county] 
+      when "payment"
+        [:payer, :payer_id, :payer_state, :transportation_type]
+      when "contact"
+        [:phone, :email, :alert_call, :alert_sms, :alert_email, :alternate_contact_name, :alternate_contact_phone, :alternate_contact_email, :alert_alternate_call, :alert_alternate_sms, :alert_alternate_email]
+      end
+
+    params.require(:patient).permit(permitted_attributes).merge(form_step: step)
+  end
+
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,8 +1,24 @@
 class Patient < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # Removed :registerable from devise to make new patient creation possible -SM
 
+  # devise :database_authenticatable, 
+         # :recoverable, :rememberable, :trackable, :validatable
+
+  cattr_accessor :form_steps do 
+    %w(basic payment contact)
+  end
   
+  attr_accessor :form_step
+
+  validates :first_name, :last_name, :address_1, :city, :state, :county, presence: true, if: -> { required_for_step?(:basic) }
+  validates :payer, :payer_id, :payer_state, :transportation_type , presence: true, if: -> { required_for_step?(:payment) }
+  validates :phone, :email, :alternate_contact_name, :alternate_contact_phone, :alternate_contact_email, presence: true, if: -> { required_for_step?(:contact) }
+
+  def required_for_step?(step)
+    return true if form_step.nil?
+    return true if self.form_steps.index(step.to_s) <= self.form_steps.index(form_step)
+  end
+
 end
