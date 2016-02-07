0b56942493513a3543dfb6b06178fee5b255377e
Sean McNally <seanmcnally88@gmail.com>
2016-02-06 20:12:54 -0600

    Fixed devise in model and routes

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,6 +1,6 @@
 require 'twilio-ruby'
 
-Twilio.configure do |configure|
+Twilio.configure do |config|
   config.account_sid = Rails.application.secrets.twilio_account_sid
   config.auth_token = Rails.application.secrets.twilio_auth_token
 end
