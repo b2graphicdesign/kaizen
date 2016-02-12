b9550e45f8edae09d56d315346e1d086b7903097
Sean McNally <seanmcnally88@gmail.com>
2016-02-12 14:32:07 -0600

    Added views for multi step driver form, added presence validations, created step controller

--- app/controllers/driver/steps_controller.rb
+++ app/controllers/driver/steps_controller.rb
@@ -1,7 +1,31 @@
 class Driver::StepsController < ApplicationController
+  include Wicked::Wizard
+  steps *Driver.form_steps
+
   def show
+    @driver = Driver.find(params[:driver_id])
+    render_wizard
   end
 
   def update
+    @driver = Driver.find(params[:driver_id])
+    @driver.update(driver_params(step))
+    render_wizard @driver
+  end
+
+  private
+
+  def driver_params(step)
+    permitted_attributes = 
+      case step
+      when "contact"
+        [:transport_id, :username, :first_name, :last_name, :address_1, :address_2, :city, :state, :zip, :phone, :email]
+      when "licensing"
+        [:insurance_company, :insurance_account, :drivers_license_state, :drivers_license_no, :drivers_license_expiry]
+      when "vehicle"
+        [:vehicle_year, :vehicle_make, :vehicle_model, :vehicle_type, :vehicle_color, :vehicle_license_plate, :vehicle_registration_expiry]
+      end
+
+    params.require(:driver).permit(permitted_attributes).merge(form_step: step)
   end
 end
--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,28 @@
 class DriversController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(
+      email: (0...8).map { (65 + rand(26)).chr }.join + "@test.com"
+      )
+    @driver.save(validate: false)
+    redirect_to driver_step_path(@driver, Driver.form_steps.first)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
