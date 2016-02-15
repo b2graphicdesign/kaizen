d40950d50f05d97e5e82bc4fb67918201c1f7e95
jhsiaomei <jw1314@gmail.com>
2016-02-15 16:10:49 -0600

    added validations to admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,7 @@ class Admin < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
 end
