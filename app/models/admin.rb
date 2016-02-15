9126ece13ec5b5a1044ac1b80035de316c2ad090
jhsiaomei <jw1314@gmail.com>
2016-02-15 16:56:19 -0600

    added validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,6 +4,8 @@ class Admin < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: { with: /@/ }
   validates :username, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\Z/ }
+  validates :username, length: { minimum: 6, message: "6 characters it the minimum allowed" }
+  validates :username, length: { maximum: 15, message: "15 characters is the maximum allowed" }
 end
