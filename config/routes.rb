cbdd108488b4304cb8546db0cd7f5c7091469379
ryanlennon <ryanlennon07@gmail.com>
2016-02-18 22:33:27 -0600

    Added routes and show pages for transportation providers and patients

--- config/routes.rb
+++ config/routes.rb
@@ -38,12 +38,14 @@ Rails.application.routes.draw do
   end
   
   get 'patient' => 'patients#index'
+  get 'patient/:id' => 'patients#show'
 
   get 'transportation' => 'transportations#index'
   get 'transportation/new' => 'transportations#new'
   post 'transportation' => 'transportations#create'
   get '/transportation/:id/edit' => 'transportations#edit'
   put '/transportation/:id' => 'transportations#update'
+  get '/transportation/:id' => 'transportations#show'
 
   get '/provider' => 'providers#index'
   get '/provider/new' => 'providers#new'
