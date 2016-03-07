7c8d20f8afa19214652f6e26b4d233962538f96f
Sean McNally <seanmcnally88@gmail.com>
2016-03-07 09:52:21 -0600

    Removed email uniqueness for admin, removed devise routes for transit_provider

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,6 @@ Rails.application.routes.draw do
   devise_for :transportations
   devise_for :patients
   devise_for :admins, :controllers => { registrations: 'registrations' }
-  devise_for :transit_providers
   devise_for :providers
 
   root 'pages#home'
