db3a115a9d92d8956eb40b56b2adcf95aa0983f4
EC2 Default User <ec2-user@ip-10-3-10-70.us-west-2.compute.internal>
2016-04-21 19:04:25 +0000

    Fixed issue with error checking of Provider add form.  Form was error checking for elements that did not exist.

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -7,12 +7,14 @@ class Provider < ActiveRecord::Base
   has_many :patients
   has_many :rides, dependent: :destroy
 
-  validates :username, :city, :state, :zip, :facility_name, :address_1, :county, :phone, :fax, presence: true
+  validates :username, presence: true
+  #validates :username, :city, :state, :zip, :facility_name, :address_1, :county, :phone, :fax, presence: true
   validates :email, format: { with: /@/, message: "must contain @" }
   validates :username, length: { minimum: 6, maximum: 15, message: "must be between 6 and 15 characters" }
-  validates :zip, :phone, :fax, length: { maximum: 15, message: "must be less than 15 characters" }
-  validates :city, :state, :address_1, :address_2, :county, length: { maximum: 25, message: "must been less than 25 characters" }
-  validates :facility_name, length: { maximum: 50, message: "must be less than 50 characters" }
+  #2016-04-19: Commented out so I can use signup form -SJD
+  #validates :zip, :phone, :fax, length: { maximum: 15, message: "must be less than 15 characters" }
+  #validates :city, :state, :address_1, :address_2, :county, length: { maximum: 25, message: "must been less than 25 characters" }
+  #validates :facility_name, length: { maximum: 50, message: "must be less than 50 characters" }
 
   def self.find_for_database_authentication(warden_conditions)
   conditions = warden_conditions.dup
